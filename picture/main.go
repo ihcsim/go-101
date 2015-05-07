4591eb98027084fc27029b9128874d95bed9b926
Ivan Sim <ivans@activestate.com>
2015-05-06 22:39:26 -0700

    Answer to go tour slice exercise.
    Question URL: http://tour.golang.org/moretypes/14

---
+++ picture/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func main() {
+  pic.Show(Pic)
+}
+
+func Pic(dx, dy int) [][]uint8 {
+  picture := make([][]uint8, dx)
+
+  for index := range picture{
+    picture[index] = make([]uint8, dy)
+  }
+
+  for x := 0; x < len(picture); x++ {
+    for y := 0; y < len(picture[x]); y++ {
+      picture[x][y] = uint8(x * y )
+    }
+  } 
+
+  return picture
+}
