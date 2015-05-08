92d916872100b06dd922fe082f622b1db21e7bc4
Ivan Sim <ivans@activestate.com>
2015-05-07 21:59:35 -0700

    Answer to maps exercise.
    http://tour.golang.org/moretypes/19

---
+++ maps/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "golang.org/x/tour/wc"
+  "strings"
+)
+
+func main(){
+  wc.Test(WordCount)  
+}
+
+func WordCount(input string) map[string]int {
+  result := make(map[string]int)
+
+  splits := strings.Fields(input)
+  for i := 0; i < len(splits); i++ {
+    count, exists := result[splits[i]]
+    if exists {
+      result[splits[i]] = count + 1
+    } else {
+      result[splits[i]] = 1
+    }
+  }
+
+  return result
+}
