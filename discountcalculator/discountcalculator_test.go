496051c0f84d59c3def60330c7ce22abf10cb10e
Ivan Sim <ivans@activestate.com>
2015-08-02 11:04:43 -0700

    Tabularized discount calculator tests.

--- discountcalculator/discountcalculator_test.go
+++ discountcalculator/discountcalculator_test.go
@@ -2,57 +2,26 @@ package discountcalculator
 
 import "testing"
 
-func TestWhenGivenAStandardCustomer_ThenReturns10PercentDiscount(t *testing.T) {
+func TestRateFor_WhenGivenCustomerType_ThenReturnsTheCorrectDiscountRateAndType(t *testing.T) {
+	var tests = []struct {
+		customerType         int
+		expectedRate         float64
+		expectedDiscountType int
+	}{
+		{customerType: STANDARD, expectedRate: 0.1, expectedDiscountType: STANDARD_DISCOUNT},
+		{customerType: SILVER, expectedRate: 0.15, expectedDiscountType: SILVER_DISCOUNT},
+		{customerType: GOLD, expectedRate: 0.2, expectedDiscountType: GOLD_DISCOUNT},
+		{customerType: PREMIUM, expectedRate: 0.25, expectedDiscountType: PREMIUM_DISCOUNT},
+	}
+
 	discountCalculator := New()
-	customer := NewCustomer(STANDARD)
-	rate := discountCalculator.RateFor(customer)
-
-	if rate != 0.1 {
-		t.Errorf("Expected discount rate to be %.2f, but get %.2f\n", 0.1, rate)
-	}
-	if discountCalculator.strategyCode != STANDARD_DISCOUNT {
-		t.Errorf("Expected calculation strategy to be %s, but get %s\n", "STANDARD", discountCalculator.strategyCode)
-	}
-}
-
-func TestWhenGivenASilverCustomer_ThenReturns15PercentDiscount(t *testing.T) {
-	discountCalculator := New()
-	customer := NewCustomer(SILVER)
-	rate := discountCalculator.RateFor(customer)
-
-	if rate != 0.15 {
-		t.Errorf("Expected discount rate to be %.2f, but get %.2f\n", 0.15, rate)
-	}
-
-	if discountCalculator.strategyCode != SILVER_DISCOUNT {
-		t.Errorf("Expected calculation strategy to be %s, but get %s\n", "SILVER", discountCalculator.strategyCode)
-	}
-}
-
-func TestWhenGivenAGoldCustomer_ThenReturns20PercentDiscount(t *testing.T) {
-	discountCalculator := New()
-	customer := NewCustomer(GOLD)
-	rate := discountCalculator.RateFor(customer)
-
-	if rate != 0.2 {
-		t.Errorf("Expected discount rate to be %.2f, but get %.2f\n", 0.2, rate)
-	}
-
-	if discountCalculator.strategyCode != GOLD_DISCOUNT {
-		t.Errorf("Expected calculation strategy to be %s, but get %s\n", "GOLD", discountCalculator.strategyCode)
-	}
-}
-
-func TestWhenGivenAPremiumCustomer_ThenReturns25PercentDiscount(t *testing.T) {
-	discountCalculator := New()
-	customer := NewCustomer(PREMIUM)
-	rate := discountCalculator.RateFor(customer)
-
-	if rate != 0.25 {
-		t.Errorf("Expected discount rate to be %.2f, but get %.2f\n", 0.25, rate)
-	}
-
-	if discountCalculator.strategyCode != PREMIUM_DISCOUNT {
-		t.Errorf("Expected calculation strategy to be %s, but get %s\n", "PREMIUM", discountCalculator.strategyCode)
+	for _, test := range tests {
+		customer := NewCustomer(test.customerType)
+		if rate := discountCalculator.RateFor(customer); rate != test.expectedRate {
+			t.Errorf("Expected discount rate to be %.2f, but get %.2f\n", test.expectedRate, rate)
+		}
+		if discountType := discountCalculator.strategyCode; discountType != test.expectedDiscountType {
+			t.Errorf("Expected calculation strategy to be %d, but get %d\n", test.expectedDiscountType, discountType)
+		}
 	}
 }
