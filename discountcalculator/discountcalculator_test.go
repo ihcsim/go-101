5f3e4db2f150b94d3a027e873df447cd06bcd841
Ivan Sim <ivans@activestate.com>
2015-08-06 21:29:54 -0700

    Change test fixture to hold customer struct type instead of int.

--- discountcalculator/discountcalculator_test.go
+++ discountcalculator/discountcalculator_test.go
@@ -4,20 +4,19 @@ import "testing"
 
 func TestRateFor_WhenGivenCustomerType_ThenReturnsTheCorrectDiscountRateAndType(t *testing.T) {
 	var tests = []struct {
-		customerType         int
+		customer             *customer
 		expectedRate         float64
 		expectedDiscountType int
 	}{
-		{customerType: STANDARD, expectedRate: 0.1, expectedDiscountType: STANDARD_DISCOUNT},
-		{customerType: SILVER, expectedRate: 0.15, expectedDiscountType: SILVER_DISCOUNT},
-		{customerType: GOLD, expectedRate: 0.2, expectedDiscountType: GOLD_DISCOUNT},
-		{customerType: PREMIUM, expectedRate: 0.25, expectedDiscountType: PREMIUM_DISCOUNT},
+		{customer: NewCustomer(STANDARD), expectedRate: 0.1, expectedDiscountType: STANDARD_DISCOUNT},
+		{customer: NewCustomer(SILVER), expectedRate: 0.15, expectedDiscountType: SILVER_DISCOUNT},
+		{customer: NewCustomer(GOLD), expectedRate: 0.2, expectedDiscountType: GOLD_DISCOUNT},
+		{customer: NewCustomer(PREMIUM), expectedRate: 0.25, expectedDiscountType: PREMIUM_DISCOUNT},
 	}
 
 	discountCalculator := New()
 	for _, test := range tests {
-		customer := NewCustomer(test.customerType)
-		if rate := discountCalculator.RateFor(customer); rate != test.expectedRate {
+		if rate := discountCalculator.RateFor(test.customer); rate != test.expectedRate {
 			t.Errorf("Expected discount rate to be %.2f, but get %.2f\n", test.expectedRate, rate)
 		}
 		if discountType := discountCalculator.strategyCode; discountType != test.expectedDiscountType {
@@ -28,21 +27,20 @@ func TestRateFor_WhenGivenCustomerType_ThenReturnsTheCorrectDiscountRateAndType(
 
 func TestRateWithCouponFor_WhenGivenCustomerAndCouponType_ThenReturnsTheCorrectDiscountRateAndType(t *testing.T) {
 	var tests = []struct {
-		customerType         int
+		customer             *customer
 		couponType           int
 		expectedRate         float64
 		expectedDiscountType int
 	}{
-		{customerType: STANDARD, couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.15, expectedDiscountType: BIRTHDAY_DISCOUNT},
-		{customerType: SILVER, couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.20, expectedDiscountType: BIRTHDAY_DISCOUNT},
-		{customerType: GOLD, couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.25, expectedDiscountType: BIRTHDAY_DISCOUNT},
-		{customerType: PREMIUM, couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.30, expectedDiscountType: BIRTHDAY_DISCOUNT},
+		{customer: NewCustomer(STANDARD), couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.15, expectedDiscountType: BIRTHDAY_DISCOUNT},
+		{customer: NewCustomer(SILVER), couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.20, expectedDiscountType: BIRTHDAY_DISCOUNT},
+		{customer: NewCustomer(GOLD), couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.25, expectedDiscountType: BIRTHDAY_DISCOUNT},
+		{customer: NewCustomer(PREMIUM), couponType: BIRTHDAY_ANNIVERSARY, expectedRate: 0.30, expectedDiscountType: BIRTHDAY_DISCOUNT},
 	}
 
 	discountCalculator := New()
 	for _, test := range tests {
-		customer := NewCustomer(test.customerType)
-		if rate := discountCalculator.RateWithCouponFor(customer, test.couponType); rate != test.expectedRate {
+		if rate := discountCalculator.RateWithCouponFor(test.customer, test.couponType); rate != test.expectedRate {
 			t.Errorf("Expected discount rate with promotion to be %.2f, but get %.2f", test.expectedRate, rate)
 		}
 
