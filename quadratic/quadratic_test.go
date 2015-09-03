ad4efde529b5d138d710c441db7a06f7581693cc
Ivan Sim <ivans@activestate.com>
2015-09-03 12:55:44 -0700

    More examples for the negative discriminant test case.

--- quadratic/quadratic_test.go
+++ quadratic/quadratic_test.go
@@ -51,12 +51,31 @@ func TestSolve_GivenZeroQuadraticCoefficent_ReturnsAnError(t *testing.T) {
 
 // Verify using calculator at http://www.mathwarehouse.com/quadratic/quadratic-formula-calculator.php
 func TestSolve_WhenDiscriminatIsNegative_ReturnsComplexNumberSolution(t *testing.T) {
+	precision := 4
 	var tests = []struct {
 		input    *equation
 		expected *solution
 	}{
-		{input: NewEquation(1.00, 2.00, 4.00, 4),
+		{input: NewEquation(1.00, 2.00, 4.00, precision),
 			expected: NewSolution(complex(-1.0000, 1.7320), complex(-1.0000, -1.7320)),
+		},
+		{input: NewEquation(2.00, 4.00, 16.00, precision),
+			expected: NewSolution(complex(-1.0000, 2.6457), complex(-1.0000, -2.6457)),
+		},
+		{input: NewEquation(-8.00, 5.00, -4.00, precision),
+			expected: NewSolution(complex(0.3125, -0.6343), complex(0.3125, 0.6343)),
+		},
+		{input: NewEquation(4.00, 10.00, 10.00, precision),
+			expected: NewSolution(complex(-1.2500, 0.9682), complex(-1.2500, -0.9682)),
+		},
+		{input: NewEquation(100.00, 81.00, 23.00, precision),
+			expected: NewSolution(complex(-0.4050, 0.2568), complex(-0.4050, -0.2568)),
+		},
+		{input: NewEquation(50.00, -24.00, 13.00, precision),
+			expected: NewSolution(complex(0.2400, 0.4498), complex(0.2400, -0.4498)),
+		},
+		{input: NewEquation(12.00, -14.00, 20.00, precision),
+			expected: NewSolution(complex(0.5833, 1.1516), complex(0.5833, -1.1516)),
 		},
 	}
 
@@ -67,11 +86,11 @@ func TestSolve_WhenDiscriminatIsNegative_ReturnsComplexNumberSolution(t *testing
 		}
 
 		if actual.x1 != test.expected.x1 {
-			t.Errorf("Expected x1 to be %.f, but got %.f", test.expected.x1, actual.x1)
+			t.Errorf("Expected x1 to be %.4f, but got %.4f", test.expected.x1, actual.x1)
 		}
 
 		if actual.x2 != test.expected.x2 {
-			t.Errorf("Expected x2 to be %.f, but got %.f", test.expected.x2, actual.x2)
+			t.Errorf("Expected x2 to be %.4f, but got %.4f", test.expected.x2, actual.x2)
 		}
 	}
 }
