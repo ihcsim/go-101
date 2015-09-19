b509d2e32072be4c7f5bccc8fbd81dab227922ef
Ivan Sim <ivan.sim@hpe.com>
2015-09-19 15:08:55 -0700

    Add main() to playlist.go

--- playlist/m3u2pls.go
+++ playlist/m3u2pls.go
@@ -15,26 +15,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 	"path/filepath"
 	"strings"
 )
-
-func main() {
-	if len(os.Args) == 1 || !strings.HasSuffix(os.Args[1], ".m3u") {
-		fmt.Printf("usage: %s <file.m3u>\n", filepath.Base(os.Args[0]))
-		os.Exit(1)
-	}
-
-	if rawBytes, err := ioutil.ReadFile(os.Args[1]); err != nil {
-		log.Fatal(err)
-	} else {
-		songs := readM3uPlaylist(string(rawBytes))
-		writePlsPlaylist(songs)
-	}
-}
 
 func readM3uPlaylist(data string) (songs []*SongRecord) {
 	var songIndex int
---
+++ playlist/playlist.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) == 1 || !strings.HasSuffix(os.Args[1], ".m3u") {
+		fmt.Printf("usage: %s <file.m3u>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	if rawBytes, err := ioutil.ReadFile(os.Args[1]); err != nil {
+		log.Fatal(err)
+	} else {
+		songs := readM3uPlaylist(string(rawBytes))
+		writePlsPlaylist(songs)
+	}
+}
